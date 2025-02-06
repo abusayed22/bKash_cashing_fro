38c5542fc041a25202f237789760ac3b1a4a4c55
abu sayed <abusayed.in22@gmail.com>
2025-02-06 21:18:20 +0600

    hum

--- components/ui/pagination.jsx
+++ components/ui/pagination.jsx
@@ -1,8 +1,9 @@
 import * as React from "react"
 import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react"
 
-import { cn } from "@/lib/utils"
+// import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/src/lib/utils";
 
 const Pagination = ({
   className,
