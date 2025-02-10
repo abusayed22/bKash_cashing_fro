4df89397e7124a882208c3fdd14935710a809b4f
abu sayed <abusayed.in22@gmail.com>
2025-02-10 13:14:50 +0600

    okay

--- src/app/auth/logout/actions/handler.js
+++ src/app/auth/logout/actions/handler.js
@@ -1,19 +1,18 @@
-'use server';
-// import { MAIN_PATH } from "@/src/utility/enviroment";
+'use client';
+import { MAIN_PATH } from "@/src/utility/enviroment";
 import { TokenDecoded } from "@/src/utility/tokenHelper";
 import { deleteCookie, getCookie } from "cookies-next";
 
 
 export const LogoutAction = async () => {
     try {
-        const cookieStore = await cookies();
-                const token = cookieStore.get('accessToken');
-        // const token = getCookie('accessToken');
+        // const {} = TokenDecoded();
+        const token = getCookie('accessToken');
         // console.log(token);
 
         const decodedToken = await TokenDecoded(token);
         const { id, email } = decodedToken;
-        const response = await fetch(`${process.env.MAIN_PATH}/auth/logout?id=${id}&email=${email}`, {
+        const response = await fetch(`${MAIN_PATH}/auth/logout?id=${id}&email=${email}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
