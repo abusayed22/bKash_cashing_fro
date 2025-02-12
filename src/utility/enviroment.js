8882fac61561cc3f781284f743a19d6bee03fdd8
abu sayed <abusayed.in22@gmail.com>
2025-02-12 11:08:15 +0600

    okay

--- src/utility/enviroment.js
+++ src/utility/enviroment.js
@@ -5,5 +5,8 @@
 // export const MAIN_PATH = 'http://localhost:3001';
 
 // vercel backend api link
-export const MAIN_PATH = 'https://b-kash-cashing-backend.vercel.app/api';
+// export const MAIN_PATH = 'https://b-kash-cashing-backend.vercel.app/api';
+
+// VPS
+export const MAIN_PATH = 'http://192.168.10.212:3000/api';
 
