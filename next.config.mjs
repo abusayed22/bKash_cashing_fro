c436c08228ab928763aded215fde6dda1f58b8ec
abu sayed <abusayed.in22@gmail.com>
2025-02-23 22:37:23 +0600

    test

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,18 @@ const nextConfig = {
             },
         ];
     },
+    async headers() {
+      return [
+          {
+              source: '/:path*', // Match all routes
+              headers: [
+                  { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, proxy-revalidate' },
+                  { key: 'Pragma', value: 'no-cache' },
+                  { key: 'Expires', value: '0' },
+              ],
+          },
+      ];
+  },
 };
 
 export default nextConfig;
