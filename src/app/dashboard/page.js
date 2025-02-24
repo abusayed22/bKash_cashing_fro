9377385c4e83adea25116039a7bd8fb442e6417d
abu sayed <abusayed.in22@gmail.com>
2025-02-24 10:23:51 +0600

    ok

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -2,20 +2,20 @@ import React from "react"
 import DashboardLayout from "@/src/layouts/DashboardLayout";
 import DashboardCard from "./_components/DashboardCard";
 import { getDashboard } from "./_actions/handler";
+import { unstable_noStore as noStore } from "next/cache";
 
 
 
 
 
-const Page = async({ params }) => {
-
-
+const Page = async ({ params }) => {
+  noStore();
   const dashboardData = await getDashboard();
   console.log(dashboardData)
   return (
     <DashboardLayout>
       <div className="container max-w-md lg:max-w-full mx-auto">
-        <DashboardCard data={dashboardData}/>
+        <DashboardCard data={dashboardData} />
       </div>
     </DashboardLayout>
   )
